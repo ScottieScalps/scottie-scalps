a5bacb6c910bd70ec32ff7c3dc4fdbca296104b2
ScottieScalps <schristie589@outlook.com>
2026-05-29 11:27:36 +0400

    Update liquidations.js

--- api/liquidations.js
+++ api/liquidations.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Asset required' });
   }
 
-  const url = `https://emea01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fapi.cryptoquant.com%2Fv1%2F%24&data=05%7C02%7C%7Cc75b70a530274ffc298108debd4eff92%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C639156344151694530%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=%2FIwKYbMmQk%2FPRtAPcf0Ez8XPtGbDStxkAR%2BMRapmW1g%3D&reserved=0{asset}/market-data/liquidations?window=min&exchange=all_exchange&limit=120`;
+  const url = `https://emea01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fapi.cryptoquant.com%2Fv1%2F%24&data=05%7C02%7C%7Cd0971a54af234ec4c65108debd53ac85%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C639156364215951635%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=EABF1xUiicUkoeuDYqScN4w3DCGItIbtSOop55i4QUw%3D&reserved=0{asset}/market-data/liquidations?window=min&exchange=all_exchange&limit=120`;
 
   try {
     const response = await fetch(url, {
@@ -18,6 +18,7 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 }
